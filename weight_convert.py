568df796506fc76c325124372f99f4e113aff339
hassan <hassan@pop-os.localdomain>
2022-09-04 14:00:50 +0300

    wrap up the app

--- weight_convert.py
+++ weight_convert.py
@@ -5,10 +5,10 @@ weight = int(input('please put your weight: '))
 forma = input(('(L) bs  or (K) g:  ')).lower()
 
 if forma == 'l':
-  new_weight = weight / 2.20462
+  new_weight = weight * 0.45
   print(f'You are {round(new_weight, 2)}  kg')
 elif forma == 'k':
-  new_weight = weight * 2.20462
+  new_weight = weight / 0.45
   print(f'You ar {round(new_weight, 2) } lbs')
 else:
   print(int(input('please put your right weight: ')))

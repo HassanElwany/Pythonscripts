2a31052802360cb9e76b995dc638675dcd0a3f76
hassan <hassan@pop-os.localdomain>
2022-09-06 12:16:33 +0300

    loop practice

--- for_loop_practice.py
+++ for_loop_practice.py
@@ -1,7 +1,7 @@
-numbers = [5, 2, 5, 2, 2]
+numbers = [5, 2, 5, 2, 2, 20, 30, 200, 101, 320]
 
-for i in numbers:
-    output = ''
-    for y in range(i):  
-      output += 'x'
-    print (output)
+numbers2 = []
+for number in numbers:
+  if number not in numbers2:
+    numbers2.append(number)
+print(numbers2)
